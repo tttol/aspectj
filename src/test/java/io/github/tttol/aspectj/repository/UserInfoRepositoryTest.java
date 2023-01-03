693f6c5da2ab742887257462b59d58cdd2026dbc
t-takahashi <tkhstol.929@gmail.com>
2023-01-03 18:29:01 +0900

    add repository test

---
+++ src/test/java/io/github/tttol/aspectj/repository/UserInfoRepositoryTest.java
@@ -0,0 +1,66 @@
+package io.github.tttol.aspectj.repository;
+
+import com.google.common.hash.Hashing;
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.Operations;
+import com.ninja_squad.dbsetup.destination.DriverManagerDestination;
+import io.github.tttol.aspectj.mbgenerate.entity.UserInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static com.ninja_squad.dbsetup.Operations.sequenceOf;
+import static com.ninja_squad.dbsetup.Operations.truncate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class UserInfoRepositoryTest {
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+    @Value("${spring.datasource.username}")
+    private String datasourceUserName;
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    @BeforeEach
+    void init() {
+        final var insertOperation = Operations.insertInto("user_info")
+                .columns("id", "name", "email")
+                .values(Hashing.sha256().hashString("1", StandardCharsets.UTF_8).toString(), "Tom", "tom@example.com")
+                .values(Hashing.sha256().hashString("2", StandardCharsets.UTF_8).toString(), "John", "john@example.com")
+                .build();
+        final var dbSetup = new DbSetup(new DriverManagerDestination(datasourceUrl, datasourceUserName, datasourcePassword),
+                sequenceOf(truncate("user_info"), insertOperation));
+        dbSetup.launch();
+    }
+
+    @Nested
+    class SelectAll {
+        @Test
+        @DisplayName("全件取得ができること")
+        void selectAllTest() {
+            final var user1 = new UserInfo();
+            user1.setId(Hashing.sha256().hashString("1", StandardCharsets.UTF_8).toString());
+            user1.setName("Tom");
+            user1.setEmail("tom@example.com");
+            final var user2 = new UserInfo();
+            user2.setId(Hashing.sha256().hashString("2", StandardCharsets.UTF_8).toString());
+            user2.setName("John");
+            user2.setEmail("john@example.com");
+            final var expected = List.of(user1, user2);
+            final var actual = userInfoRepository.selectAll();
+
+            assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+        }
+    }
+}
