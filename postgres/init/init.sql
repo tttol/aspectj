4f3e5341989532345f8277cc9af9a6fde480a53e
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-05-20 16:12:34 +0900

    apply `spring-boot-docker-compose`

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE aspectj_demo;
 GRANT ALL PRIVILEGES ON DATABASE aspectj_demo TO postgres;
+CREATE DATABASE aspectj_demo_test;
+GRANT ALL PRIVILEGES ON DATABASE aspectj_demo_test TO postgres;
